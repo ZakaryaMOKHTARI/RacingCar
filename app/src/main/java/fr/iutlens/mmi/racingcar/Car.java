a60c78d9fe2dbc363b5e388abff09c1ef127c991
zakarya.mokhtari <mokhtari.zak@gmail.com>
2018-03-20 11:21:37 +0100

    Modif de la carte, ajout de graphisme / ralentissement dans l'herbe et interdiction de sortir du décor

--- app/src/main/java/fr/iutlens/mmi/racingcar/Car.java
+++ app/src/main/java/fr/iutlens/mmi/racingcar/Car.java
@@ -29,6 +29,7 @@ public class Car {
     public void paint(Canvas canvas, int unit_x, int unit_y){
         canvas.save();
         canvas.translate(x*unit_x,y*unit_y);
+        canvas.scale(2,2);
         canvas.rotate(direction);
         int model = 0;
         if (dd < -5) model = 1;
@@ -38,11 +39,26 @@ public class Car {
     }
 
 
-    public void update() {
-        direction += dd*v*sprite.h;
-        x += (float) ( v*sprite.h*Math.cos(Math.toRadians(direction-90)));
-        y += (float) ( v*sprite.h*Math.sin(Math.toRadians(direction-90)));
+    public void update(Track track) {
 
+        if(!track.valide(x, y)){ // je peux le remplacer par if get == 1
+
+            v = v/2;
+
+
+
+        }
+
+
+        direction += dd*v*sprite.h;
+       // acceleration =
+
+        float oldx =x;
+        x += (float) ( v*sprite.h*Math.cos(Math.toRadians(direction-90)));
+        if (x<0.5 || x >= track.getSizeX()-0.5) {
+            x = oldx;
+        }
+        y += (float) ( v*sprite.h*Math.sin(Math.toRadians(direction-90)));
     }
 
     public double bound(double value, double max){
@@ -58,12 +74,19 @@ public class Car {
     }
 
     public void setCommand(double pitch, double roll) {
-        pitch = rescale(pitch,150,5);
+
+
+        pitch = rescale(pitch,100,5); // le pitch max représente la vitesse de la voiture au départ (150 de base)
         roll = rescale(roll,40,50);
 
-        this.v = (float) (pitch*0.00005);
-        this.dd = 0;
-        this.dd = (float) (roll/2);
+        this.v = (float) (pitch*0.00005*1.7);
+
+        this.dd = (float) (roll*0.6);
 
     }
+
+
+
+//    System.currentTimeMillis();
+
 }
